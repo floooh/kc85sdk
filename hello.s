3ad18afdc0dcdb30535a60636eca1986373fe0b8
Andre Weissflog <floooh@gmail.com>
2014-11-09 01:13:57 +0100

    simplified hello.s

--- hello.s
+++ hello.s
@@ -1,19 +1,14 @@
-; simple Hello World in z80 assembler
+; simple KC85 Hello World in Z80 assembler
 
 ; KC programs usually start at address 0x200
 	org 0x200
 
 ; the KC-specific program header
-	db 0x7f
-	db 0x7f
-	dm "HELLO"
-	db 01
+	db 0x7F,0x7F,"HELLO",1 
 
 ; execution starts here
-	call 0xf003		; CAOS system call
-	db 0x23			; system call number (print string)
-	dm "HELLO WORLD!"	; embedded string
-	db 0xD, 0xA		; new-line
-	db 0x0			; string terminated with 0-byte
+	call 0xF003			; CAOS system call
+	db 0x23				; system call number (print string)
+	db "HELLO WORLD!\r\n",0		; embedded 0-terminated string
 	ret
 
