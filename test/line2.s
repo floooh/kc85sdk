4cf207186c0adca8a16f49f3c0e642534e93c52b
Andre Weissflog <floooh@gmail.com>
2014-11-27 20:55:04 +0100

    Added vertical line case

--- test/line2.s
+++ test/line2.s
@@ -4,19 +4,21 @@
         org $200
 end_mask:   
         db $80,$C0,$E0,$F0,$F8,$FC,$FE,$FF
+start_mask:
+        db $80,$40,$20,$10,$8,$4,$2,$1 
 
         db $7f,$7f,"LINE2",$1        
-        ld hl,$807f
+        ld hl,$8080
         ld de,$ff00
 line_l0:
         push hl
         push de
-        call line_hori
+        call line
         pop de
         pop hl
-        inc e
-        ld a,$ff
-        cp e
+        dec d
+        xor a
+        cp d 
         jp nz,line_l0
 
         ld hl,$807f
@@ -24,7 +26,7 @@ line_l0:
 line_l1:
         push hl
         push de
-        call line_hori
+        call line
         pop de
         pop hl
         inc e
@@ -32,7 +34,56 @@ line_l1:
         cp e
         jp nz,line_l1
 
+        ld hl,$8080
+        ld de,$00ff
+line_l2:
+        push hl
+        push de
+        call line
+        pop de
+        pop hl
+        inc d
+        ld a,$ff
+        cp d
+        jp nz,line_l2
+
+        ld hl,$8080
+        ld de,$ffff
+
+line_l3:
+        push hl
+        push de
+        call line
+        pop de
+        pop hl
+        dec e
+        xor a
+        cp e
+        jp nz,line_l3
+
         ret
+
+
+;-------------------------------------------------------------------------------
+;   line
+;   hl: x0y0
+;   de: x1y1
+;
+line:           ; check if line_hori or line_vert must be called
+        ld a,d  ; compute dx,dy and compare
+        sub h
+        jp nc,pos_dx
+        neg a
+pos_dx:
+        ld b,a
+
+        ld a,e
+        sub l
+        jp nc,pos_dy
+        neg a
+pos_dy:
+        cp b    ; dy - dx
+        jp nc, line_vert
         
 ;-------------------------------------------------------------------------------
 ;   line_hori
@@ -109,7 +160,7 @@ dy_cont:
         srl a       
         
 patch_jp:  
-        jr pixel_loop   ; line-start jump target patched by above code
+        jr loop_x       ; line-start jump target patched by above code
 
 ;   a: error accumulator
 ;   c: current pixel mask
@@ -117,7 +168,7 @@ patch_jp:
 ;   e: dy 
 ;  hl: vidmem address
 ;--- handle 8 pixel bits in unrolled loop
-pixel_loop:
+loop_x:
 ;--- bit7 (left-most)
         set 7,c             ; 2 bytes +
         sub e               ; 1 byte +
@@ -164,10 +215,10 @@ cont0:  ld a,(hl)           ; write current pixel
         ld c,a
         inc h               ; increment x
         ex af,af'           ; restore error AND carry flag (!)
-        jp nc,pixel_loop    ; this was an end-of-column
+        jp nc,loop_x        ; this was an end-of-column
         add d               ; carry flag was set: error correction, and increment y
 p6:     inc l
-        jp pixel_loop
+        jp loop_x
 bmp_y:  
         add d           ; error correction (err + dx)
         ex af,af'       ; store current error
@@ -195,4 +246,86 @@ done1:
 p4:     and $ff         ; final bitmask will be patched at start
         ld (hl),a
         ret
+       
+;------------------------------------------------------------------------------
+;   line_vert
+;   hl: x0y0
+;   de: x1y1
+line_vert:
+        ld a,e      ; make sure that y1 >= y0
+        cp l
+        jp nc, line_y0y1
+
+line_y1y0:
+        ex de,hl    ; if y0 > y1, exchange x0y0 <-> x1y1
+
+line_y0y1:
+        push de     ; store x1y1 (we need y1 in b later)
+        ld a,e      ; compute dy -> e
+        sub l
+        ld e,a
         
+        ld a,d      ; compute dx -> d
+        sub h
+        ld d,a
+        jp nc, dx_pos   ; dx is positive
+dx_neg:             ; line goes to the left, neg dx and patch code to rotate pixel mask left
+        neg a
+        ld d,a
+        ld a,$1     ; RLC c, second byte 
+        ld (p7+1),a
+        ld a,$25    ; dec h
+        ld (p8),a
+        jp dx_cont
+dx_pos:             ; line goes to the right, patch code to rotate pixel mask right
+        ld a,$9     ; RRC c, second byte
+        ld (p7+1),a
+        ld a,$24    ; inc h
+        ld (p8),a
+dx_cont:
+        ld a,h      ; initial pixel mask
+        and $7
+        ld bc,start_mask
+        add c
+        ld c,a
+        ld a,(bc)        
+        pop bc      ; bc is x1y1
+        ld b,c      ; b is y1 (for line end check)
+        ld c,a      ; c is initial pixel mask
+
+        ld a,h      ; compute vidmem address into hl
+        srl a
+        srl a
+        srl a
+        add $80
+        ld h,a      ; hl: $8xyy
+
+        ld a,e      ; compute initial error accumulator (dy / 2)
+        srl a
+
+;   a: error accumulator
+;   c: current pixel mask
+;   d: dx
+;   e: dy
+;  hl: vidmem address
+loop_y:
+        ex af,af'           ; store current error accumulator
+        ld a,l              ; line-end check
+        cp b                ; b is y1
+        ret z               ; we're done
+        ld a,(hl)           ; write current pixel
+        xor c
+        ld (hl),a
+        inc l               ; increment y-coord
+        ex af,af'           ; restore error accumulator
+        sub d
+        jp nc,loop_y
+        add e               ; correct error
+p7:     rrc c               ; rotate pixel mask, will be code patch for line to left/right
+        jp nc,loop_y
+p8:     inc h               ; increment (or decrement) x-column
+        jp loop_y
+               
+
+        
+ 
